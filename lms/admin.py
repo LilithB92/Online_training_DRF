c9bd0682da6bdd97471cc880760bef08b4f69d39
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-11 16:58:19 +0300

    lesson model and its admin is added

--- lms/admin.py
+++ lms/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from lms.models import Course
+from lms.models import Lesson
 
 
 # Register your models here.
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "preview", "description")
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "preview", "description", "video_url", "course")
--- lms/models.py
+++ lms/models.py
@@ -13,3 +13,18 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание урока", blank=True)
+    preview = models.ImageField(upload_to="lessons/previews/", verbose_name="Превью", blank=True, null=True)
+    video_url = models.URLField(verbose_name="Ссылка на видео", blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
