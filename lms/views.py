9f33bde127bb5551762fe6b8936ecea7025360ca
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-12 15:11:12 +0300

    CRUD for Lesson is added in views.py

--- lms/views.py
+++ lms/views.py
@@ -1,11 +1,53 @@
 from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import UpdateAPIView
 
 from lms.models import Course
+from lms.models import Lesson
 from lms.serializers import CourseSerializer
+from lms.serializers import LessonSerializer
 
 
 class CourseViewSet(viewsets.ViewSet):
     """CRUD для курса"""
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
+class LessonCreateApiView(CreateAPIView):
+    """Создание одной сущности урока"""
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(ListAPIView):
+    """Получение списка уроков"""
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    """Получение одной сущности урока"""
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    """Изменение одной сущности урока"""
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    """Удаление одной сущности урока"""
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
