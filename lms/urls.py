71e36b00dcbbe3b86653718dd90c3c4655363c68
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-12 15:27:24 +0300

    add urls for Lesson CRUD

--- lms/urls.py
+++ lms/urls.py
@@ -1,11 +1,18 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from lms.apps import LmsConfig
-from lms.views import CourseViewSet
+from lms.views import CourseViewSet, LessonListApiView, LessonUpdateAPIView, LessonCreateApiView, LessonDestroyAPIView, LessonRetrieveAPIView
 
 app_name = LmsConfig.name
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="courses"),
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("lessons/", LessonListApiView.as_view()),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view()),
+    path("lessons/create/", LessonCreateApiView.as_view()),
+    path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view()),
+    path("lessons/<int:pk>/delete", LessonDestroyAPIView.as_view()),
+              ] + router.urls
