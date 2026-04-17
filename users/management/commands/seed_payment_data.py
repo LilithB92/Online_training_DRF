fe52f2a67d434838145e93e9da87d8115fb54377
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-18 00:21:34 +0300

    Set up filtering(course, lesson, payment_method) for the payment list

--- users/management/commands/seed_payment_data.py
+++ users/management/commands/seed_payment_data.py
@@ -11,11 +11,14 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         user, _ = User.objects.get_or_create(username="john", password="johnpassword", email="john@example.com")
         course, _ = Course.objects.get_or_create(title="python", description="good course")
+        course1, _ = Course.objects.get_or_create(title="php", description="good course")
 
         payments = [
             {"user": user, "course": course, "amount": 57000.50, "payment_method": "cash"},
-            {"user": user, "course": course, "amount": 25000.50, "payment_method": "cash"},
+            {"user": user, "course": course1, "amount": 25000.50, "payment_method": "cash"},
             {"user": user, "course": course, "amount": 30000.50, "payment_method": "cash"},
+            {"user": user, "course": course1, "amount": 12000.50, "payment_method": "transfer"},
+            {"user": user, "course": course, "amount": 83000.50, "payment_method": "transfer"},
         ]
 
         for payment_data in payments:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from users.models import Payment
@@ -6,5 +7,8 @@ from users.serializers import PaymentSerializer
 
 class PaymentViewSet(viewsets.ModelViewSet):
     """Получение CRUD Платежи"""
+
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["course", "lesson", "payment_method"]
