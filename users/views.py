4172259be71df18b55385812720beb24db617d20
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-18 00:33:01 +0300

    set up ordering by payment_date for the paymentlist

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import viewsets
 
 from users.models import Payment
@@ -10,5 +11,6 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ["course", "lesson", "payment_method"]
+    ordering_fields = ['payment_date']
