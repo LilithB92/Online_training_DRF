600fc2e98d9bab36c22c549f8460161118459924
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-26 20:26:35 +0300

    added owner field in lessons and course models

--- lms/models.py
+++ lms/models.py
@@ -1,15 +1,24 @@
+from django.conf import settings
 from django.db import models
 
 
 class Course(models.Model):
     """
-    Stores a single course entry, related to :model:`lms.Lesson`
+    Stores a single course entry, related to :model:`lms.Lesson` and :model:`users.User`
     """
 
     title = models.CharField(max_length=200, verbose_name="Название курса")
     # Поле для картинки, сохраняется в MEDIA_ROOT/courses/
     preview = models.ImageField(upload_to="courses/", verbose_name="Превью", blank=True, null=True)
     description = models.TextField(verbose_name="Описание курса", blank=True, null=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Владелец",
+        related_name="courses",
+    )
 
     class Meta:
         verbose_name = "Курс"
@@ -21,7 +30,7 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     """
-    Stores a single lesson entry, related to :model:`lms.Course`
+    Stores a single lesson entry, related to :model:`lms.Course` and :model:`users.User`.
     """
 
     title = models.CharField(max_length=200, verbose_name="Название урока")
@@ -29,6 +38,14 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to="lessons/previews/", verbose_name="Превью", blank=True, null=True)
     video_url = models.URLField(verbose_name="Ссылка на видео", blank=True, null=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс", related_name="lessons")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Владелец",
+        related_name="lessons",
+    )
 
     def __str__(self):
         return self.title
