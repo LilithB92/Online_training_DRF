8ccee5f0fb9acd4e10bfd05d21675b4dba834ae6
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-26 15:07:16 +0300

    Command for creating superuser is added

---
+++ users/management/commands/create_superuser.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = "Создать супер юзера"
+
+    def handle(self, *args, **options):
+        user = User.objects.create(email="admin@mail.com")
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password("admin1111")
+
+        if user:
+            self.stdout.write(self.style.SUCCESS(f"Successfully added superuser: {user.__str__()}"))
+        else:
+            self.stdout.write(self.style.WARNING(f"Superuser already exists: {user.__str__()}"))
