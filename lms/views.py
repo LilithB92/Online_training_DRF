7298c7e2d09fbc19486130b512d023be305b76d2
Lilith <bichakhchyanlilith1992@gmail.com>
2026-05-01 19:32:36 +0300

    some fixes SubscriptionAPIView

--- lms/views.py
+++ lms/views.py
@@ -1,12 +1,17 @@
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from lms.models import Course
+from lms.models import CourseSubscription
 from lms.models import Lesson
 from lms.paginators import LessonCoursePagination
 from lms.serializers import CourseDetailSerializer
@@ -93,3 +98,37 @@ class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, ~IsModerator | IsOwner)
+
+
+class SubscriptionAPIView(APIView):
+    """ API View для управления подпиской пользователя на курс.
+
+    Работает в режиме 'toggle' (переключатель): если подписки нет — создает,
+    если есть — удаляет.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, *args, **kwargs):
+        """
+      Обрабатывает POST-запрос на изменение статуса подписки.
+      Ожидает в body:
+      {
+          "course": <int:id_курса>
+      }
+      """
+        user = self.request.user
+        course_id = self.request.data.get('course_id')
+        if not course_id:
+            return Response(
+                {"error": "course_id is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        course_item = get_object_or_404(Course, id=course_id)
+        subs_item = CourseSubscription.objects.filter(user=user, course=course_item)
+        if subs_item.exists():
+            subs_item.delete()
+            message = 'подписка удалена'
+        else:
+            CourseSubscription.objects.create(user=user, course=course_item)
+            message = 'подписка добавлена'
+        return Response({"message": message}, status=status.HTTP_200_OK)
